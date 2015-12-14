9907a41e64473536f165f944061644ce8a94cc74
Alex Conlin-Oakley <alex@grabyo.com>
2015-12-14 09:48:50 +0000

    add psqlrc

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_aliases vimrc vim zshrc oh-my-zsh private git.scmbrc scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
+files="bashrc bash_aliases vimrc vim zshrc oh-my-zsh private git.scmbrc scrotwm.conf Xresources psqlrc"    # list of files/folders to symlink in homedir
 
 ##########
 
